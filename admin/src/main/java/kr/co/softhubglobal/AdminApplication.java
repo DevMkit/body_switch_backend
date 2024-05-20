115370b9b2a82125af74300b72c5ceda722eb675
Temuujin <temka0118@yahoo.com>
2024-05-20 15:07:51 +0800

    [ALL] - Initial setup

---
+++ admin/src/main/java/kr/co/softhubglobal/AdminApplication.java
@@ -0,0 +1,11 @@
+package kr.co.softhubglobal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
