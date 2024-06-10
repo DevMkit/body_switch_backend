11f5e545236b06ce1cc4a767a2361439a4b343d4
Temuujin <temka0118@yahoo.com>
2024-06-10 14:16:54 +0800

    [common] - PageableDTO added & message properties updated

---
+++ common/src/main/java/kr/co/softhubglobal/dto/PageableDTO.java
@@ -0,0 +1,26 @@
+package kr.co.softhubglobal.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+public class PageableDTO {
+
+    @Data
+    public static class Request {
+        private int page = 0;
+        private int limit = 10;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Response {
+        private Long totalElements;
+        private Integer size;
+        private Integer number;
+        private List<?> content;
+    }
+}
+
